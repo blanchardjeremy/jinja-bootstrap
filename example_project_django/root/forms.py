a1c17aac04ec316b2378cda17db9ed761f94d58f
Jeremy Blanchard <auzigog@gmail.com>
2012-03-10 14:53:10 -0800

    Add a bunch of form examples

--- example_project_django/root/forms.py
+++ example_project_django/root/forms.py
@@ -1,6 +1,14 @@
 from django import forms
+from django.forms.widgets import Input
+
+class EmailInput(Input):
+    input_type = 'email'
 
 class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
+
+class InlineForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput())
 
@@ -10,3 +18,11 @@ class RegisterForm(forms.Form):
     email = forms.CharField(help_text="You will be asked to verify your email address")
     password1 = forms.CharField(label="Password", help_text="Must be at least 32 characters long and contain exactly 7 unicode characters", widget=forms.PasswordInput())
     password2 = forms.CharField(label="Verify password", help_text="Enter your password again for verificaiton!", widget=forms.PasswordInput())
+
+
+class FancyForm(forms.Form):
+    email = forms.CharField(required=False, label="HTML5 Email Input", help_text="Try typing in a wrong email address and pressing submit", widget=EmailInput())
+    placeholder1 = forms.CharField(required=False, label="Placeholder in template", widget=forms.Textarea())
+    placeholder2 = forms.CharField(required=False, label="Placeholder in form.py", widget=forms.Textarea(attrs={'rows': 4, 'placeholder': 'This is defines in root/forms.py instead of the template'}))
+    fake_error = forms.CharField(label="Error Field")
+    long_field = forms.CharField(required=False, label="Long field")
--- example_project_django/root/views.py
+++ example_project_django/root/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.base import TemplateView
-from root.forms import LoginForm, RegisterForm
+from root.forms import *
 from jingo.helpers import *
 
 class HomeView(TemplateView):
@@ -8,10 +8,20 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context_data = super(HomeView, self).get_context_data(**kwargs)
 
+        fake_data_to_force_error = {
+            'fake_error': '', # required field with no data
+        }
+        fancy_form = FancyForm(data=fake_data_to_force_error)
+
+
         data = {
             'login_form': LoginForm(),
+            'inline_form': InlineForm(),
             'register_form': RegisterForm(),
+            'fancy_form': fancy_form,
         }
+
+
 
         context_data.update(data)
         return context_data
